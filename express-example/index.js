54fe2604e3393254a00b11567c50c6699637cda6
Sergio Gomez <sergio.gomez@atomic32.com>
2022-08-23 21:09:05 -0500

    request and responses

--- express-example/index.js
+++ express-example/index.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const fs = require('fs');
 
 const app = express()
 // curl --verbose http://localhost:4001/fisting_pup
 // the route path can be anything you want
 app.get('/fisting_pup', (req, reply) => {
-    reply.send('Services did its job')
+    
+    if(req.headers["accept"] === "image/jpeg") {
+
+        fs.readFile('/Users/desktop/background.jpeg', 'utf8', (err, data) => {
+            if (err) {
+                reply.statusCode(400)
+                reply.send(err.message)
+                return;
+            }
+            // reply.setHeader('content-type', 'application/json')
+            reply.send({"data": 1})
+        
+        });
+    } else {
+        reply.statusCode(405)
+        reply.send('I only send image/jpeg I dont send: ' + req.headers["accept"])
+        // THIS setHeader line wont be sent back as part of the response because reply.send(...) will commit the response and send it back
+        reply.setHeader('x-daddys-response', 'im here')
+    }
+
 })
 // curl --verbose http://localhost:4001/async_services
 app.get('/asyn_services', async (req) => {
